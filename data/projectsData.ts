07ed1dfafd28fec858da726260291650412e5058
Duarte Cancela <duartecancela@gmail.com>
2025-08-04 16:29:09 +0100

    udate projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,20 +12,10 @@ type ProjectsData = {
 const projectsData: ProjectsData = {
   en: [
     {
-      title: 'A Search Engine',
-      description: `What if you could look up any information in the world? Webpages, images, videos
-        and more. Google has many features to help you find exactly what you're looking
-        for.`,
-      imgSrc: '/static/images/google.png',
-      href: 'https://www.google.com',
-    },
-    {
-      title: 'The Time Machine',
-      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-        to the desired date and press "Go". No more worrying about lost keys or
-        forgotten headphones with this simple yet affordable solution.`,
-      imgSrc: '/static/images/time-machine.jpg',
-      href: '/blog/the-time-machine',
+      title: 'Electronic Components Stock Manager',
+      description: `Tired of losing track of all your sensors and modules? This Laravel-powered app helps you manage your electronic parts inventory, with real-time stock tracking and a built-in API. Ideal for makers, hobbyists, and electronics enthusiasts.`,
+      imgSrc: '/static/images/stock-manager.png',
+      href: '/blog/electronic-components-stock-manager',
     },
   ],
 
